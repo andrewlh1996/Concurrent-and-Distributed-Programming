e3cc73adac628254f88ee1d9ec2af2279288b62d
Andrei Leahu <andrei.leahu@kireygroup.com>
2021-05-15 17:55:51 +0300

    Add google translate service

--- Project3/google-cloud-project/src/app/app.component.ts
+++ Project3/google-cloud-project/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoogleObj, GoogleService } from './google.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'google-cloud-project';
+export class AppComponent implements OnInit {
+  //Translate
+  public googleObj: GoogleObj = new GoogleObj();
+  public key: string = 'AIzaSyD9EyBKBy4ZrXI42wOcVY6siGca-_O9EmU';
+  public result = '';
+  private btnSubmit: any;
+
+  constructor(private _google: GoogleService,) { }
+
+  ngOnInit() {
+    this.googleObj.source = 'en';
+    this.googleObj.target = 'es';
+    this.btnSubmit = document.getElementById('btnSubmit');
+  }
+
+  send() {
+    this.btnSubmit.disabled = true;
+    this._google.translate(this.googleObj, this.key).subscribe(
+      (res: any) => {
+        this.btnSubmit.disabled = false;
+        this.result = res.data.translations[0].translatedText;
+        console.log(this.googleObj.source,this.googleObj.target,res.data.translations[0].translatedText);
+
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
 }
--- Project3/google-cloud-project/src/app/app.module.ts
+++ Project3/google-cloud-project/src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleService } from './google.services';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatSelectModule,
   ],
-  providers: [],
+  providers: [GoogleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
